8adb33d57f8b37a616525c32eb1c339d4297060f
Mtalkhnani <mtalkhnani@hotmail.com>
2022-12-13 19:27:50 +0300

    a

---
+++ my_app/lib/screens/profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ProfileScreen extends StatelessWidget {
+  static const String id = 'profile_screen';
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: ,
+        child: ,
+      )
+    );
+  }
+}
